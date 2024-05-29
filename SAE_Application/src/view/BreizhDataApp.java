9c02b6abceccaa0dfffbcd5a14ae3e2f7f5957b3
Malliwi <william.wall114@gmail.com>
2024-05-29 11:33:31 +0200

    commit

--- SAE_Application/src/view/BreizhDataApp.java
+++ SAE_Application/src/view/BreizhDataApp.java
@@ -1,19 +1,32 @@
 package view;
+import control.BDFirstController;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-import model.dao.AeroportDAO;
-import model.data.Aeroport;
-import java.util.List;
+public class BreizhDataApp extends Application {
 
-public class BreizhDataApp {
+    private BDFirstController controller;
+    private IntroScene introScene;
+    
+
+    public void start(Stage primaryStage) {
+        this.controller = new BDFirstController(primaryStage, this);
+        this.introScene = new IntroScene(controller);
+        Scene scene = new Scene(introScene, 1500, 1000);
+
+        primaryStage.setTitle("BreizhData");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
-        
-        AeroportDAO aeroportDAO = new AeroportDAO();
-
-        List<Aeroport> aeroports = aeroportDAO.findAll();
-
-        for (Aeroport aeroport : aeroports) {
-            System.out.println(aeroport);
-        }
+        launch(args);
     }
+
+    public IntroScene getIntroScene() {
+        return this.introScene;
+    }
+
 }
+
---
+++ SAE_Application/src/view/ConnexionScene.java
@@ -0,0 +1,63 @@
+package view;
+
+import control.BDFirstController;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class ConnexionScene {
+
+    private Stage stage;
+    private BDFirstController controller;
+    private VBox layout;
+    private TextField usernameField;
+    private PasswordField passwordField;
+    private Button loginButton;
+    private Button backButton;
+    private Label messageLabel;
+
+
+    public ConnexionScene(BDFirstController controller) {
+        initialize();
+    }
+
+    private void initialize() {
+        layout = new VBox();
+        layout.setPadding(new Insets(10));
+        layout.setSpacing(10);
+
+        usernameField = new TextField();
+        usernameField.setPromptText("Nom d'utilisateur");
+
+        passwordField = new PasswordField();
+        passwordField.setPromptText("Mot de passe");
+
+        loginButton = new Button("Se connecter");
+        loginButton.getStyleClass().add("button-start");
+
+        backButton = new Button("Retour");
+        backButton.getStyleClass().add("button-back");
+
+        messageLabel = new Label();
+        messageLabel.setStyle("-fx-text-fill: red;");
+
+        layout.getChildren().addAll(usernameField, passwordField, loginButton, backButton, messageLabel);
+    }
+
+
+    public Button getLoginButton() {
+        return loginButton;
+    }
+
+    public Button getBackButton() {
+        return backButton;
+    }
+
+    public Stage getStage() {
+        return stage;
+    }
+}
